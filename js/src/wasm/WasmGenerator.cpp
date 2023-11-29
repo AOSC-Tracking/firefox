38248041410eae700cdc59e1fcd5c0f96528b3da
Jiangjin Wang <the@salted.fish>
2023-11-28 21:44:16 -0800

    Implement SummarizeTrapInstruction for MIPS64
    Co-Authored-By: Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>

--- js/src/wasm/WasmGenerator.cpp
+++ js/src/wasm/WasmGenerator.cpp
@@ -792,7 +792,7 @@ static void CheckCodeBlock(const CodeBlock& codeBlock) {
 
 #  if (defined(JS_CODEGEN_X64) || defined(JS_CODEGEN_X86) ||   \
        defined(JS_CODEGEN_ARM64) || defined(JS_CODEGEN_ARM) || \
-       defined(JS_CODEGEN_LOONG64))
+       defined(JS_CODEGEN_LOONG64) || defined(JS_CODEGEN_MIPS64))
   // Check that each trapsite is associated with a plausible instruction.  The
   // required instruction kind depends on the trapsite kind.
   //

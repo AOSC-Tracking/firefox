c583fe32fb950218f8ad2de52c6fc34e4f51946e
Mingcong Bai <jeffbai@aosc.io>
2024-04-05 22:52:21 +0800

    feat(netwerk): declare AOSC OS in user agent string

--- netwerk/protocol/http/nsHttpHandler.cpp
+++ netwerk/protocol/http/nsHttpHandler.cpp
@@ -940,6 +940,7 @@ void nsHttpHandler::BuildUserAgent() {
   mUserAgent.SetCapacity(mLegacyAppName.Length() + mLegacyAppVersion.Length() +
 #ifndef UA_SPARE_PLATFORM
                          mPlatform.Length() +
+                         mPlatform.Length() + 9 +
 #endif
                          mOscpu.Length() + mMisc.Length() + mProduct.Length() +
                          mProductSub.Length() + mAppName.Length() +
@@ -958,6 +959,7 @@ void nsHttpHandler::BuildUserAgent() {
   if (!mPlatform.IsEmpty()) {
     mUserAgent += mPlatform;
     mUserAgent.AppendLiteral("; ");
+    mUserAgent.AppendLiteral("AOSC OS; ");
   }
 #endif
   if (!mCompatDevice.IsEmpty()) {
